ffdd97c0061b00c282c6a431e488d5b441ac9358
Florian Unternaehrer <justus.jonas@gmail.com>
2012-06-26 10:56:38 +0200

    copied contents of master branch to gh-pagesso I can have a hosted version of minekeepr on github

---
+++ js/minekeepr.js
@@ -0,0 +1,233 @@
+$(function() {
+
+  Minekeepr = {};
+
+  Minekeepr.Field = function(x, y, board) {
+
+    this.hasBomb = false;
+
+    this.addBomb = function() {
+      this.hasBomb = true;
+    };
+
+    this.getSurroundingFields = function() {
+      this.surroundingFields = this.surroundingFields || _.chain([y-1, y, y+1])
+          .map(function(rowId) {
+              return _([x-1, x, x+1]).map(function(columnId) {
+                return rowId != y && columnId != x && board.fields[rowId] && board.fields[rowId][columnId] || null;
+              });
+            })
+          .flatten()
+          .compact(function(field) { return field != null; })
+          .value();
+      return this.surroundingFields;
+    };
+
+    this.countSurroundingBombs = function() {
+      this.surroundingBombsCount = this.surroundingBombsCount || _(this.getSurroundingFields()).filter(function(item) {
+        return item.hasBomb;
+      }).length;
+      return this.surroundingBombsCount;
+    };
+
+    this.hit = function(ev) {
+      if (board.clockStarted == null) {
+        board.startGame();
+      }
+
+      if (this.hasBomb) {
+        board.stopGame();
+        board.explode(x, y);
+      } else {
+        this.expose();
+      }
+    };
+
+    this.markBomb = function() {
+      if (this.marked) {
+        var character = this.exposed && this.countSurroundingBombs() > 0 && this.countSurroundingBombs() || "-";
+        this.marked = false;
+        this.el.removeClass("marked");
+        this.el.html(character);        
+        this.el.on('click', this.hit.bind(this));
+      } else {
+        if (!this.exposed && !this.hidden) {
+          this.marked = true;
+          this.el.addClass("marked");
+          this.el.html("o");
+          this.el.off("click");
+        }
+      }
+
+      return false;
+    };
+
+    this.expose = function() {
+      this.exposed = true;
+      $(this.el).addClass("exposed");
+
+      if (!this.hidden) {        
+        if (this.countSurroundingBombs() == 0) {
+          $(this.el).addClass("empty");
+          var neighbours = _(this.getSurroundingFields()).filter(function(field) {
+            return !field.exposed;
+          });
+
+          _(neighbours).each(function(field) {
+            field.hit();
+          });
+        } else {
+          this.el.html(this.countSurroundingBombs());
+        }
+      }
+    };
+
+    this.explode = function() {
+      this.el.addClass("exploded");
+      if (this.hasBomb) { 
+        this.el.addClass("bomb");
+        this.el.html("x");
+      } else {
+        this.el.html("-");
+      }
+      this.el.off("click");
+    };
+
+    this.hide = function() {
+      this.el.addClass("exploded");
+
+      setTimeout(function() {
+        this.hidden = true;
+        this.el.html("#");
+      }.bind(this), 500);
+    };
+
+    this.render = function() {
+      this.el = $("<a>").html("-")
+                        .on('click', this.hit.bind(this))
+                        .on("contextmenu", this.markBomb.bind(this));
+      return this.el;
+    };
+
+    return this;
+  };
+
+  Minekeepr.Board = function(board_width, bombs_count) {
+    var board_width = board_width || 30,
+        bombs_count = bombs_count || 100,
+
+        generate = function() {
+          this.fields = _.range(board_width).map(function(x) { 
+            return _.range(board_width).map(function(y) {
+              return new Minekeepr.Field(x, y, this);
+            }.bind(this));
+          }.bind(this));
+
+          _(bombs_count).times(function() {
+            addRandomBomb();
+          });
+        }.bind(this),
+
+        getRandomField = function() {
+          var x = Math.floor(Math.random() * board_width),
+              y = Math.floor(Math.random() * board_width);
+          return this.fields[x][y];
+        }.bind(this),
+
+        addRandomBomb = function() {
+          var field = getRandomField();
+          field.hasBomb && addRandomBomb() || field.addBomb();
+        }.bind(this),
+
+        startClock = function() {
+          var value = 999;
+
+          $('#clock').html(value);
+          value = value - 1;
+          this.clockInverval = setInterval(function() {
+            $('#clock').html(value);
+            value = value - 1;
+          }, 100);
+
+          this.clockStarted = true;
+        },
+
+        stopClock = function() {
+          clearInterval(this.clockInverval);
+        },
+
+        startGlitch = function() {          
+          this.glitchInterval = setInterval(hideRandomField, 1000);
+        },
+
+        stopGlitch = function() {
+          clearInterval(this.glitchInterval);
+        },
+
+        hideRandomField = function() {
+          var field = getRandomField();
+          if (field.exposed || field.marked) {
+            hideRandomField();
+          } else {
+            field.hide();
+          }
+        };
+
+    this.explode = function(x_center, y_center, i) {
+      var i = i || 0;
+
+      _(_.range(x_center - i, x_center + i)).each(function(x) {
+        if (x >= 0 && x < board_width) {
+          _(_.range(y_center - i, y_center + i)).each(function(y) {
+            if (y >= 0 && y < board_width) {
+              this.fields[x][y].explode();
+            }
+          }.bind(this));
+        }
+      }.bind(this));
+
+      if (i < board_width) {
+        // TODO: add easing to animation
+        setTimeout(function() {
+          this.explode(x_center, y_center, i + 1);
+        }.bind(this), 10);
+      }
+    };
+
+    this.startGame = function() {
+      if (!this.started) {
+        startClock();
+        // startGlitch();
+        this.started = true;
+      }
+    };
+
+    this.stopGame = function() {
+      stopClock();
+      stopGlitch();
+    };
+
+    this.render = function() {
+      var element = $("<div>");
+
+      _(this.fields).each(function(row) {
+        _(row).each(function(field) {
+          element.append(field.render());
+        });
+
+        element.append("<br>");
+      });
+
+      return element;
+    }.bind(this);
+
+    generate();
+    return this
+  };
+
+
+  // kickstart
+  board = new Minekeepr.Board;
+  $("#minekeepr").html(board.render());
+
+});
